c5dadd9e98460b2e3edee84ce3de8b93caf8c5fe
Filippo Sallemi <filippo.sallemi@gdcgroup.com>
2025-04-05 15:15:05 +0200

    fix user relationship and add .gitignore to storage upload

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -44,6 +45,14 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Get the disks for the user.
+     */
+    public function disks(): HasMany
+    {
+        return $this->hasMany(Disk::class);
     }
 
     /**
