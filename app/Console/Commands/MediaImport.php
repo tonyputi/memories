3f9e54903f2c3bc11d3dedbe19350464f7cbfd80
Filippo Sallemi <filippo.sallemi@gdcgroup.com>
2025-04-05 17:40:44 +0200

    add basic job to import archive

--- app/Console/Commands/MediaImport.php
+++ app/Console/Commands/MediaImport.php
@@ -2,15 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Jobs\ProcessUploadedArchive;
 use App\Models\Disk;
-use App\Models\Medium;
-use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Console\PromptsForMissingInput;
-use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
-use ZipArchive;
 
 use function Laravel\Prompts\search;
 
@@ -21,7 +17,7 @@ class MediaImport extends Command implements PromptsForMissingInput
      *
      * @var string
      */
-    protected $signature = 'app:media-import {user} {disk} {file}';
+    protected $signature = 'app:media-import {disk} {file}';
 
     /**
      * The console command description.
@@ -38,13 +34,6 @@ class MediaImport extends Command implements PromptsForMissingInput
     protected function promptForMissingArgumentsUsing(): array
     {
         return [
-            'user' => fn () => search(
-                label: 'Which user should receive the imported media?',
-                placeholder: 'Search for a user...',
-                options: fn ($v) => strlen($v) > 0
-                    ? User::where('name', 'like', "%{$v}%")->pluck('name', 'id')->all()
-                    : [],
-            ),
             'disk' => fn () => search(
                 label: 'Which disk should the media be imported to?',
                 placeholder: 'Search for a disk...',
@@ -56,9 +45,8 @@ class MediaImport extends Command implements PromptsForMissingInput
                 label: 'Which file should be imported?',
                 placeholder: 'Search for a file...',
                 options: fn ($v) => strlen($v) > 0
-                    ? Storage::disk('uploads')->files()
+                    ? Storage::disk('uploads')->allFiles()
                     : [],
-                transform: fn ($v) => Storage::disk('uploads')->path($v),
             ),
         ];
     }
@@ -68,95 +56,6 @@ class MediaImport extends Command implements PromptsForMissingInput
      */
     public function handle()
     {
-        $user = User::find($this->argument('user'));
-        $disk = Disk::find($this->argument('disk'));
-        $file = $this->argument('file');
-
-        $storage = $disk->storage();
-
-        if (! file_exists($file)) {
-            $this->error('File does not exist...');
-
-            return;
-        }
-
-        if (mime_content_type($file) !== 'application/zip') {
-            $this->error('File is not a zip file...');
-
-            return;
-        }
-
-        $name = basename($file);
-
-        $this->info("Importing media from {$name}...");
-
-        $tempStorage = $this->extractZip($user, $file);
-
-        [$metas, $files] = collect($tempStorage->allFiles())
-            ->partition(fn ($file) => str_ends_with($file, '.json'));
-
-        $this->info("Importing {$files->count()} files...");
-
-        $bar = $this->output->createProgressBar($files->count());
-
-        foreach ($files as $file) {
-            if ($meta = $metas->first(fn ($meta) => $meta === "{$file}.json")) {
-                $meta = json_decode($tempStorage->get($meta), true);
-                if (json_last_error() !== JSON_ERROR_NONE) {
-                    $this->error('Meta is not a valid JSON file...');
-                    $meta = [];
-                }
-            }
-
-            $name = basename($file);
-
-            $extension = pathinfo($file, PATHINFO_EXTENSION);
-            $uuid = Str::lower(Str::random(32).'.'.$extension);
-            $hash = hash_file('sha256', $tempStorage->path($file));
-            $createdAt = $tempStorage->lastModified($file);
-            $updatedAt = $tempStorage->lastModified($file);
-
-            if (! copy($tempStorage->path($file), $storage->path($uuid))) {
-                $this->error("Failed to copy file {$file} to {$uuid}...");
-
-                continue;
-            }
-
-            Medium::updateOrCreate([
-                'user_id' => $user->getKey(),
-                'disk_id' => $disk->getKey(),
-                'name' => $name,
-                'path' => $uuid,
-                'hash' => $hash,
-            ], [
-                'meta' => $meta ?? [],
-                'created_at' => $createdAt,
-                'updated_at' => $updatedAt,
-            ]);
-
-            $bar->advance();
-        }
-
-        $bar->finish();
-
-        if ($tempStorage->deleteDirectory($user->getKey())) {
-            $this->info('Temporary storage deleted successfully!');
-        } else {
-            $this->error('Failed to delete temporary storage...');
-        }
-
-        $this->info('Media imported successfully!');
-    }
-
-    protected function extractZip($user, $file): Filesystem
-    {
-        $storage = Storage::disk('uploads');
-
-        $zip = new ZipArchive;
-        $zip->open($file);
-        $zip->extractTo($storage->path($user->getKey()));
-        $zip->close();
-
-        return $storage;
+        ProcessUploadedArchive::dispatchSync($this->argument('file'), $this->argument('disk'));
     }
 }
---
+++ app/Jobs/ProcessUploadedArchive.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Disk;
+use App\Models\Medium;
+use Exception;
+use Illuminate\Contracts\Filesystem\Filesystem;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use ZipArchive;
+
+class ProcessUploadedArchive implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public string $path, public string $disk_id)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $disk = Disk::findOrFail($this->disk_id);
+        $tempDirectory = dirname($this->path);
+
+        try {
+            $tempStorage = Storage::disk('uploads');
+
+            if ($tempStorage->mimeType($this->path) !== 'application/zip') {
+                Log::error('Invalid archive...');
+
+                return;
+            }
+
+            if (! $this->extractZip($tempStorage, $this->path)) {
+                Log::error('Failed to extract archive...');
+
+                return;
+            }
+
+            if (! $tempStorage->delete($this->path)) {
+                Log::error('Failed to delete temporary storage...');
+
+                return;
+            }
+
+            // TODO: files non deve contenere file inderiderati come .gitignore o .DS_Store etc
+            [$metas, $files] = collect($tempStorage->allFiles())
+                ->partition(fn ($file) => Str::endsWith($file, '.json'));
+
+            $storage = $disk->storage();
+
+            Log::info('Processing files...');
+
+            foreach ($files as $file) {
+                $meta = [];
+
+                if ($metaFile = $metas->first(fn ($meta) => $meta === "{$file}.json")) {
+                    $meta = json_decode($tempStorage->get($metaFile), true);
+                }
+
+                $name = basename($file);
+
+                $extension = pathinfo($file, PATHINFO_EXTENSION);
+                $hash = md5_file($tempStorage->path($file));
+                $path = Str::lower("$hash.$extension");
+
+                $createdAt = $tempStorage->lastModified($file);
+                $updatedAt = $tempStorage->lastModified($file);
+
+                if (! copy($tempStorage->path($file), $storage->path($path))) {
+                    Log::error("Failed to copy file {$file} to {$path}...");
+
+                    continue;
+                }
+
+                Medium::updateOrCreate([
+                    'user_id' => $disk->user_id,
+                    'disk_id' => $disk->getKey(),
+                    'name' => $name,
+                    'hash' => $hash,
+                    'path' => $path,
+                ], [
+                    'meta' => $meta ?? [],
+                    'created_at' => $createdAt,
+                    'updated_at' => $updatedAt,
+                ]);
+
+                Log::info("File {$file} processed...");
+            }
+
+            if (! $tempStorage->deleteDirectory($tempDirectory)) {
+                Log::error('Failed to delete temporary storage...');
+            }
+        } catch (Exception $e) {
+            Log::error('Failed to process uploaded archive...');
+            if (! $tempStorage->deleteDirectory($tempDirectory)) {
+                Log::error('Failed to delete temporary storage...');
+            }
+        }
+    }
+
+    protected function extractZip(Filesystem $storage, string $path): bool
+    {
+        $zip = new ZipArchive;
+        $zip->open($storage->path($path));
+        $zip->extractTo($storage->path(dirname($path)));
+        $zip->close();
+
+        return true;
+    }
+}
