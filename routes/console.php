56d905b39df42391310db2d4a9d1f4208fc99baf
Filippo Sallemi <filippo.sallemi@gdcgroup.com>
2025-04-07 13:06:48 +0200

    add disks purge command

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Models\Disk;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('disks:purge', function () {
+    Disk::withTrashed()->each(fn (Disk $disk) => $disk->forceDelete());
+    Storage::disk('local')->deleteDirectory('*');
+    Storage::disk('public')->deleteDirectory('*');
+});
