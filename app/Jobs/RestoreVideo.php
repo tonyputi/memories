8150116e3cd6bf95e8116df1180ec7a19e1aedb6
Filippo Sallemi <filippo.sallemi@gdcgroup.com>
2025-04-09 07:37:30 +0200

    add basic video processor

--- app/Jobs/RestoreVideo.php
+++ app/Jobs/RestoreVideo.php
@@ -5,7 +5,9 @@ namespace App\Jobs;
 use App\Models\Disk;
 use App\Models\Medium;
 use Carbon\Carbon;
+use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -30,7 +32,7 @@ class RestoreVideo extends RestoreMedium
             return;
         }
 
-        $meta = [];
+        $meta = $this->getMeta($uploads);
         $hash = md5_file($uploads->path($this->path));
         $path = Str::lower(sprintf('%s.%s', $hash, pathinfo($this->path, PATHINFO_EXTENSION)));
 
@@ -50,5 +52,55 @@ class RestoreVideo extends RestoreMedium
             'created_at' => Carbon::createFromTimestamp(data_get($meta, 'taken_at') ?? $uploads->lastModified($this->path)),
             'updated_at' => Carbon::createFromTimestamp(data_get($meta, 'taken_at') ?? $uploads->lastModified($this->path)),
         ]);
+    }
+
+    public function getMeta(Filesystem $uploads): array
+    {
+        $ffprobe = $this->getMetaFromFFProbe($uploads->path($this->path));
+
+        $meta = [];
+        foreach ($ffprobe['streams'] as $stream) {
+            if ($stream['codec_type'] === 'video') {
+                $meta['width'] = $stream['width'];
+                $meta['height'] = $stream['height'];
+                if (isset($stream['side_data_list'])) {
+                    foreach ($stream['side_data_list'] as $item) {
+                        if (isset($item['rotation'])) {
+                            $meta['orientation'] = intval($item['rotation']);
+                            break;
+                        }
+                    }
+                }
+    
+                if (isset($stream['tags']['rotate'])) {
+                    $meta['orientation'] = intval($stream['tags']['rotate']);
+                }
+                break;
+            }
+        }
+
+        $location = rtrim(data_get($ffprobe, 'format.tags.location'), '/');
+
+        if (preg_match('/^([+-][0-9.]+)([+-][0-9.]+)$/', $location, $matches)) {
+            $meta['gps'] = [
+                'lat' => floatval($matches[1]),
+                'lng' => floatval($matches[2]),
+            ];
+        }
+
+        return $meta;
+    }
+
+    public function getMetaFromFFProbe(string $path): array
+    {
+        $command = sprintf('ffprobe -v quiet -print_format json -show_format -show_streams "%s"', $path);
+        $result = Process::command($command)->run();
+
+        if (! $result->successful()) {
+            Log::error("Failed to get metadata for {$path} using ffprobe...");
+            return [];
+        }
+
+        return json_decode($result->output(), true);
     }
 }
